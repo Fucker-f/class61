c6647f2fdaaf4849fd953ab78be723c3a9ab7df2
fangchenwei <749838395@qq.com>
2019-03-07 10:00:31 +0800

    项目初始化

---
+++ hello/src/main/java/cn/itcast/hello/Hello.java
@@ -0,0 +1,13 @@
+package cn.itcast.hello;
+/*
+@author  Fucker
+@version   v1.0
+@date   2019/2/13 20:36
+@description TODO
+*/
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("helloa12");
+    }
+}
